7ee6b509a26988405efa7961fce2eca881453d7a
Lukáš Kmoch <lukas.kmoch@designeo.cz>
2019-03-29 13:40:27 +0100

    First commit

---
+++ AspNetCoreBlazorExamples.Web/Program.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Blazor.Hosting;
+
+namespace AspNetCoreBlazorExamples.Web
+{
+    public class Program
+    {
+        // https://medium.freecodecamp.org/how-to-create-an-application-using-blazor-and-entity-framework-core-1c1679d87c7e
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IWebAssemblyHostBuilder CreateHostBuilder(string[] args) =>
+            BlazorWebAssemblyHost.CreateDefaultBuilder()
+                .UseBlazorStartup<Startup>();
+    }
+}
